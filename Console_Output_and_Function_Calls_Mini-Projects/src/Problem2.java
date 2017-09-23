0e49735d95f26748344e45cc5a86b98cfbe7aa84
Programming <Programming@DESKTOP-AGP4T7F>
2017-09-22 22:29:17 -0400

    initial commit for problem 2

---
+++ Console_Output_and_Function_Calls_Mini-Projects/src/Problem2.java
@@ -0,0 +1,9 @@
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		System.out.println("A well-formed Java program has \na main method with { and } \nbraces.");
+		System.out.println("\nA System.out.println statement \nhas ( and ) and usually a \nString enclosed between \" and \"");
+	}
+
+}
