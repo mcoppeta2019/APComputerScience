4c642a0e76aaeefe7672c3f41c6da4616c813c90
mcoppeta2019 <mcoppeta2019@LAB-206-04.central.cchs>
2017-10-12 11:19:13 -0400

    creates the problem 2 file

---
+++ CoppetaWhileLoopProjects/src/Problem2.java
@@ -0,0 +1,17 @@
+/*
+ * 	Project 2 (4-6, page 147)
+ * 	Michael Coppeta
+ * 
+ * 	This program predicts population growth based on input
+ */
+
+import java.util.Scanner; //instantiates the scanner
+
+public class Problem2 {
+
+	public static void main(String[] args){
+		int initialOrganisms, growthRate, hoursForRate, hours;
+		
+		
+	}
+}
