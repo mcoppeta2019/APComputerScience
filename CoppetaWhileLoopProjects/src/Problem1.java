f88b2dfb631b6ba81cf9be3baa49ad6fb3891ac4
mcoppeta2019 <mcoppeta2019@LAB-206-04.central.cchs>
2017-10-12 10:49:19 -0400

    finished problem 1

--- CoppetaWhileLoopProjects/src/Problem1.java
+++ CoppetaWhileLoopProjects/src/Problem1.java
@@ -1,4 +1,20 @@
+/*
+ * 	Problem 1
+ * 	Michael Coppeta
+ * 
+ * 	This program finds the first number whose sum of the digits preceding
+ * 	it is greater than 100
+ */
 
 public class Problem1 {
-
+	public static void main(String[] args){
+		int n = 1, sum = 0;
+		
+		while (sum<=1000000){
+			sum+=n;
+			n++;
+		}
+		
+		System.out.println("The number is " + (n-1));
+	}
 }
