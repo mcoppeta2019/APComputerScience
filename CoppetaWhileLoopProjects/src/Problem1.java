2dfd64a0905a642270ceb618b7ac0a21ee177e57
mcoppeta2019 <mcoppeta2019@LAB-206-04.central.cchs>
2017-10-12 10:57:33 -0400

    added comments

--- CoppetaWhileLoopProjects/src/Problem1.java
+++ CoppetaWhileLoopProjects/src/Problem1.java
@@ -7,14 +7,17 @@
  */
 
 public class Problem1 {
+	
 	public static void main(String[] args){
-		int n = 1, sum = 0;
 		
-		while (sum<=1000000){
-			sum+=n;
-			n++;
+		int n = 1, sum = 0; //initializes two variables to hold the number, and to hold the sum
+		
+		while (sum<=1000000){ //the sum must be greater than 1000000
+			sum+=n; //it adds the current number to the sum
+			n++; //increases n by 1
 		}
 		
+		//prints the result to the user
 		System.out.println("The number is " + (n-1));
 	}
 }
