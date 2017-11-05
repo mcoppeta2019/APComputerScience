db7022a700dfd8ac7baf726ee33b012450eeaa0d
Programming <Programming@DESKTOP-AGP4T7F>
2017-11-05 14:33:36 -0500

    finished problem 8, fixed problem 7

--- CoppetaStringProjects/src/Problem7.java
+++ CoppetaStringProjects/src/Problem7.java
@@ -20,15 +20,12 @@ public class Problem7 {
 		
 		input.close(); //closes the scanner
 		
-		//prints part of the output
-		System.out.print("Your name in reverse order is ");
-		
-		//prints the returned value of processName method with name as parameter
-		System.out.println(processName(name) + ".");
+		//calls processName method, sends name as parameter, the method prints the result
+		processName(name);
 	}
 	
-	//this method takes a name as a parameter, then returns the name in the lastname, firstname format
-	public static String processName(String name){
+	//this method takes a name as a parameter, then prints the name in the lastname, firstname format
+	public static void processName(String name){
 		
 		//stores first name in variable
 		String firstname = name.substring(0, name.indexOf(" ")); //first name is the first char to the space
@@ -36,7 +33,10 @@ public class Problem7 {
 		//stores last name in variable
 		String lastname = name.substring(name.indexOf(" ") + 1, name.length()); //last name is the text after the space
 		
-		//returns the name in lastname, firstname format
-		return (lastname + ", " + firstname);
+		//prints part of the output
+		System.out.print("Your name in reverse order is ");
+				
+		//prints the names in the reverse order
+		System.out.println(lastname + ", " + firstname + ".");
 	}
 }
---
+++ CoppetaStringProjects/src/Problem8.java
@@ -0,0 +1,41 @@
+/*
+ * 	Problem 8
+ * 	Michael Coppeta
+ * 
+ * 	This program takes user input, and prints the input is reverse
+ */
+
+import java.util.Scanner; //imports the scanner
+
+public class Problem8 {
+
+	//main method gets user input, and prints the return value of printReverse method
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in); //instantiates the scanner
+		String text; //declares variable to hold user input
+		
+		//gets user input
+		System.out.print("Please enter a string: "); //prompts user for input
+		text = input.nextLine(); //stores input in text variable
+		
+		input.close(); //closes the scanner
+		
+		//calls method that prints the text in reverse order, takes text as a parameter
+		printReverse(text);
+	}
+	
+	//this method takes text as a parameter, reverses the string, and prints it
+	public static void printReverse(String text){
+		String printText = ""; //declares new variable for the text to be printed
+		
+		//loops through each character, last to first, and adds them to a new string
+		for(int i = text.length() - 1; i >= 0; i--){
+			
+			//adds the current character of the loop to the printText variable
+			printText += text.charAt(i);
+		}
+		
+		//prints the reversed text 
+		System.out.println(printText);
+	}
+}
