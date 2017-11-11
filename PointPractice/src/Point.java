9019a25f800820094bf86b93337fa0f691e7e0bd
Programming <Programming@DESKTOP-AGP4T7F>
2017-11-10 23:12:37 -0500

    finished point class, still needs a few tests

--- PointPractice/src/Point.java
+++ PointPractice/src/Point.java
@@ -1,7 +1,76 @@
-
+/*
+ * 	Point Class
+ * 	Michael Coppeta
+ * 
+ * 	Creates class file for Point
+ */
 public class Point {
 
 	//Instance Variables
 	double x, y;
+	
+	//CONSTRUCTORS
+	
+	//Default
+	public Point(){
+		this(0, 0); //sets instance variables equal to 0, the origin
+	}
+	
+	//Given Points
+	public Point(double newX, double newY){
+		x = newX; //sets instance variables equal to the parameters of the constructor
+		y = newY;
+	}
+	
+	//Given a Point Object
+	public Point(Point oldPoint){
+		this(oldPoint.x, oldPoint.y); //sets instance variable equal to the instance variables of the given object
+	}
+	
+	//ACCESSORS
+	
+	//Accessor for X 
+	public double getX(){
+		return x;
+	}
+	
+	//Accessor for Y
+	public double getY(){
+		return y;
+	}
+	
+	//MUTATORS
+	
+	//Mutator for X
+	public void setX(double newX){
+		x = newX;
+	}
+	
+	//Mutator for Y
+	public void setY(double newY){
+		y = newY;
+	}
+	
+	//BEHAVIORS
+	
+	//setLocation method, sets instance variables equal to the given pair
+	public void setLocation(double newX, double newY){
+		x = newX;
+		y = newY;
+	}
+	
+	//translate method, adjusts the point's x and y by the given amounts
+	public void translate(double dx, double dy){
+		x += dx;
+		y += dy;
+	}
+	
+	//distance method, returns the point's distance from the given point
+	public double distance(Point p){
+		return Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2)); // (dx^2 + dy^2)^(1/2)
+	}
+	
+	
+
 }
 
--- PointPractice/src/PointTest.java
+++ PointPractice/src/PointTest.java
@@ -4,5 +4,32 @@ public class PointTest {
 	//Main method to test Point Class
 	public static void main(String[] args){
 		
+		//tests constructors
+		Point p1 = new Point();
+		Point p2 = new Point(3,4);
+		Point p3 = new Point(p2);
+		
+		//tests accessors
+		System.out.println(p1.getX() + " " + p1.getY());
+		System.out.println(p2.getX() + " " + p2.getY());
+		System.out.println(p3.getX() + " " + p3.getY());
+		
+		
+		//tests mutators
+		p2.setX(6);
+		p2.setY(8);
+		System.out.println(p2.getX());
+		System.out.println(p2.getY());
+		
+		//tests distance()
+		System.out.println(p1.distance(p2));
+		System.out.println(p1.distance(p3));
+		
+		//tests translate() and setLocation()
+		p2.translate(3,4);
+		p3.setLocation(9,12);
+		
+		System.out.println(p2.getX() + " " + p2.getY());
+		System.out.println(p3.getX() + " " + p3.getY());
 	}
 }
