e64db7a1cc53c6a0237add6e2694c4bc57896114
Michael Coppeta <mcoppeta@gmail.com>
2018-03-12 22:09:37 -0400

    update

--- CoppetaInheritanceProjects/src/BWTV.java
+++ CoppetaInheritanceProjects/src/BWTV.java
@@ -4,10 +4,12 @@
 
 public class BWTV extends TV implements Analog {
 
+	//rotateRabbitEars method returns a message unique to the type of TV
 	public String rotateRabbitEars() {
 		return "Rabbit ears rotated 45 degrees";
 	}
 
+	//returns the type of TV
 	public String tvType() {
 		return "I'm a black and white TV";
 	}
--- CoppetaInheritanceProjects/src/HDTV.java
+++ CoppetaInheritanceProjects/src/HDTV.java
@@ -4,10 +4,13 @@
 
 public class HDTV extends TV implements Digital {
 
+	
+	//connectHDMI method returns a message unique to the type of TV
 	public String connectHDMI() {
 		return "HDMI cable connected";
 	}
 
+	//returns the type of TV
 	public String tvType() {
 		return "I'm a High Def 24 million color TV";
 	}
